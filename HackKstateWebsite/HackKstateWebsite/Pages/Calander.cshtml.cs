38d20539182f300182ea4c6f5b9fbdbda580532e
acrow18 <acrow18@usd112.org>
2022-11-12 05:13:30 -0600

    Added Files from other solution

---
+++ HackKstateWebsite/HackKstateWebsite/Pages/Calander.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HackKstateWebsite.Pages
+{
+    public class CalanderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
