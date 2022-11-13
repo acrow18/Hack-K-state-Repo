a3210215413e5fa42d76306d6ef78b7ed6b0a421
acrow18 <acrow18@usd112.org>
2022-11-13 08:10:06 -0600

    Pushing code to finish Other resources

---
+++ HackKstateWebsite/HackKstateWebsite/Pages/OtherResources.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HackKstateWebsite.Pages
+{
+    public class OtherResourcesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
