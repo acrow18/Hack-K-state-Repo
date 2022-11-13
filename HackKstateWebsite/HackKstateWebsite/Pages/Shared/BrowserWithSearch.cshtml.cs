1861514f769c511286458bfbc90e43662dccb9cc
acrow18 <acrow18@usd112.org>
2022-11-12 22:42:48 -0600

    Added Brower with search to test

---
+++ HackKstateWebsite/HackKstateWebsite/Pages/Shared/BrowserWithSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HackKstateWebsite.Pages.Shared
+{
+    public class BrowserWithSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HackKstateWebsite/HackKstateWebsite/wwwroot/js/browse.js
@@ -0,0 +1,16 @@
+﻿/*code from  */
+// JavaScript code
+function search_browse() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('collegelist');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
