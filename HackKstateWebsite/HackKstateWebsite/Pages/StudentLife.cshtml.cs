92c13818b375d92659d922d10366bd35227b3eae
acrow18 <acrow18@usd112.org>
2022-11-13 07:16:51 -0600

    Pushing so others can work on the code

---
+++ HackKstateWebsite/HackKstateWebsite/Pages/StudentLife.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HackKstateWebsite.Pages
+{
+    public class StudentLifeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
